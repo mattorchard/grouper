e1f9e667cfecf7aa79b46eee2e2f0eecfc90f709
Matt Orchard <morch028@uottawa.ca>
2023-11-12 17:20:57 -0500

    Add catch to auto run

--- src/auto-run.ts
+++ src/auto-run.ts
@@ -1,5 +1,9 @@
 import { autoRunIfEnabled } from "./helpers/extensionHelpers";
 
-autoRunIfEnabled().then((didRun) => {
-  if (didRun) console.log("Auto-run complete");
-});
+autoRunIfEnabled()
+  .then((didRun) => {
+    if (didRun) console.log("Auto-run complete");
+  })
+  .catch((error) => {
+    console.error("Failed to auto-run", error);
+  });
